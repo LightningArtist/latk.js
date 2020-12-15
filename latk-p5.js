227e3da8fd3340eacb6ad7cffa9545d136008b21
Nick Fox-Gieg <nick@fox-gieg.com>
2020-12-15 00:15:50 -0500

    conversion functions

--- latk-p5.js
+++ latk-p5.js
@@ -33,11 +33,45 @@ class LatkP5 extends Latk {
 	}
 
 	convertLatkToP5() {
-		// TODO
+		this.globalScale = createVector(this.globalScale[0], this.globalScale[1], this.globalScale[2]);
+		this.globalOffset = createVector(this.globalOffset[0], this.globalOffset[1], this.globalOffset[2]);
+
+		for (let layer of this.layers) {
+			for (let frame of layer.frames) {
+				frame.parent_location = createVector(frame.parent_location[0], frame.parent_location[1], frame.parent_location[2]);
+				
+				for (let stroke of frame.strokes) {
+					stroke.color = color(stroke.color[0], stroke.color[1], stroke.color[2], stroke.color[3]);
+					stroke.fill_color = color(stroke.fill_color[0], stroke.fill_color[1], stroke.fill_color[2], stroke.fill_color[3]);
+					
+					for (let point of frame.points) {
+						point.co = createVector(point.co[0], point.co[1], point.co[2]);
+						point.vertex_color = color(point.vertex_color[0], point.vertex_color[1], point.vertex_color[2], point.vertex_color[3]);
+					}
+				}
+			}
+		}
 	}
 
 	convertP5ToLatk() {
-		// TODO
+		this.globalScale = [ this.globalScale.x, this.globalScale.y, this.globalScale.z ];
+		this.globalOffset = [ this.globalOffset.x, this.globalOffset.y, this.globalOffset.z ];
+
+		for (let layer of this.layers) {
+			for (let frame of layer.frames) {
+				frame.parent_location = [ frame.parent_location.x, frame.parent_location.y, frame.parent_location.z ];
+
+				for (let stroke of frame.strokes) {
+					stroke.color = [ red(stroke.color), green(stroke.color), blue(stroke.color), alpha(stroke.color) ];
+					stroke.fill_color = [ red(stroke.fill_color), green(stroke.fill_color), blue(stroke.fill_color), alpha(stroke.fill_color) ];
+
+					for (let point of frame.points) {
+						point.co = [ point.co.x, point.co.y, point.co.z ];
+						point.vertex_color = [ red(point.vertex_color), green(point.vertex_color), blue(point.vertex_color), alpha(point.vertex_color) ];
+					}
+				}
+			}
+		}
 	}
 
 }
--- latk-three.js
+++ latk-three.js
@@ -33,11 +33,45 @@ class LatkThree extends Latk {
 	}
 
 	convertLatkToThree() {
-		// TODO
+		this.globalScale = new THREE.Vector3(this.globalScale[0], this.globalScale[1], this.globalScale[2]);
+		this.globalOffset = new THREE.Vector3(this.globalOffset[0], this.globalOffset[1], this.globalOffset[2]);
+
+		for (let layer of this.layers) {
+			for (let frame of layer.frames) {
+				frame.parent_location = new THREE.Vector3(frame.parent_location[0], frame.parent_location[1], frame.parent_location[2]);
+
+				for (let stroke of frame.strokes) {
+					stroke.color = new THREE.Color(stroke.color[0], stroke.color[1], stroke.color[2], stroke.color[3]);
+					stroke.fill_color = new THREE.Color(stroke.fill_color[0], stroke.fill_color[1], stroke.fill_color[2], stroke.fill_color[3]);
+					
+					for (let point of frame.points) {
+						point.co = new THREE.Vector3(point.co[0], point.co[1], point.co[2]);
+						point.vertex_color = new THREE.Color(point.vertex_color[0], point.vertex_color[1], point.vertex_color[2], point.vertex_color[3]);
+					}
+				}
+			}
+		}
 	}
 
 	convertThreeToLatk() {
-		// TODO
+		this.globalScale = [ this.globalScale.x, this.globalScale.y, this.globalScale.z ];
+		this.globalOffset = [ this.globalOffset.x, this.globalOffset.y, this.globalOffset.z ];
+
+		for (let layer of this.layers) {
+			for (let frame of layer.frames) {
+				frame.parent_location = [ frame.parent_location.x, frame.parent_location.y, frame.parent_location.z ];
+
+				for (let stroke of frame.strokes) {
+					stroke.color = [ stroke.color.r, stroke.color.g, stroke.color.b, stroke.color.a ];
+					stroke.fill_color = [ stroke.fill_color.r, stroke.fill_color.g, stroke.fill_color.b, stroke.fill_color.a ];
+					
+					for (let point of frame.points) {
+						point.co = [ point.co.x, point.co.y, point.co.z ];
+						point.vertex_color = [ point.vertex_color.r, point.vertex_color.g, point.vertex_color.b, point.vertex_color.a ];
+					}
+				}
+			}
+		}
 	}
 
 }
